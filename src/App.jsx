6b961ba4db460dcd79a9e7f6bcc17146f376c93c
YuyaYamazi <kaimomo12345@gmail.com>
2023-11-01 17:03:14 +0900

    apiでurlを取得

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,40 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { getAllPokemon } from './components/pokemon'
+import { getAllPokemon,getPokemon } from './components/pokemon'
 
 function App() {
  const jsonURL =  'https://pokeapi.co/api/v2/pokemon'
- const [loadingStr, setStr] = useState('ロード中')
+ const [loading, setLoading] = useState('ロード中...')
+ const [pokemonsData, setPokemonsData] = useState([])
 
  useEffect(() => {
   const fetchPokemonData = async () => {
     // すべてのポケモンデータ
-    let res = await getAllPokemon(jsonURL)
-    console.log(res)
-    setStr('ポケモン情報を取得しました')
+    let res = await getAllPokemon(jsonURL);
+    // 詳細データの所得
+   allPokemon(res.results)
+    setLoading('ポケモンデータ取得完了')
   }
-  fetchPokemonData()
+  setTimeout(() => {
+    fetchPokemonData()
+  }, 2000);
  },[])
 
+ const allPokemon = async (data) => {
+  let pokemonData = await Promise.all(
+    data.map((pokemon) => {
+     let pokemonArray = getPokemon(pokemon.url)
+     return pokemonArray
+    })
+  )
+  setPokemonsData(pokemonData)
+ }
+console.log(pokemonsData)
 
   return (
     <>
-      <h1>{ loadingStr }</h1>
+      <h1 className='text-center text-yellow-400 text-4xl'>{ loading }</h1>
     </>
   )
 }
--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -5,3 +5,11 @@ export const getAllPokemon = (url) => {
         .then((data) => resolve(data))
     })
 }
+
+export const getPokemon = (url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+        .then((res) => res.json())
+        .then((data) => resolve(data))
+    })
+}
