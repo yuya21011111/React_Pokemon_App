2f951fa9edd82a44bc7ecce81208d1041acfba9a
YuyaYamazi <kaimomo12345@gmail.com>
2023-11-05 17:00:42 +0900

    Google翻訳API導入

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react"
+import axios from 'axios';
 const Card = ({ pokemon }) => {
+  const [name, setName] = useState(pokemon.name)
+  const handleTranslation = async () => {
+    const apiKey = 'AIzaSyA63uX2cdhI8odLq1MWmTlhovD5xbPQVfU'; // 自分のAPIキーに置き換えてください
+
+    try {
+      const response = await axios.post(
+        `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`,
+        {
+          q: pokemon.name,
+          target: 'ja' // 翻訳先の言語コード（例: 'ja'は日本語）
+        }
+      );
+      const translatedText = response.data.data.translations[0].translatedText;
+     setName(translatedText)
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  handleTranslation();
     return  (
         <>
           <div className="mb-4 mt-4 mx-auto text-black bg-gray-100 font-bold border shadow-2xl">
             <div className="flex justify-center">
               <img src={pokemon.sprites.front_default} alt="" className="transtion duration-1000 transform hover:scale-150 w-60" />
             </div>
-            <h3 className="cardName text-3xl">{pokemon.name}</h3>
+            <h3 className="cardName text-3xl">{ name }</h3>
             <div className="cardTypes">
               <div className="mb-4">タイプ</div>
               {pokemon.types.map((type, i) => {
