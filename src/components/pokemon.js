2ea14f09ad311e4383b2256667b72d1201d4da51
YuyaYamazi <kaimomo12345@gmail.com>
2023-11-05 17:22:31 +0900

    gitignore

---
+++ src/components/pokemon.js
@@ -0,0 +1,15 @@
+export const getAllPokemon = (url) => {
+    return new Promise((resolve, reject) => {
+     fetch(url)
+        .then((res) => res.json())
+        .then((data) => resolve(data))
+    })
+}
+
+export const getPokemon = (url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+        .then((res) => res.json())
+        .then((data) => resolve(data))
+    })
+}
